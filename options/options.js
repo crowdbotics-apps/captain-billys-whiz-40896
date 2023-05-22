6dd1cefe33901954f6f90898afe603583a4091b9
Crowdbotics <team@crowdbotics.com>
2023-05-22 19:30:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "captain-billys-whiz-40896",
+  url: "https://captain-billys-whiz-40896.botics.co",
+  api: "https://captain-billys-whiz-40896.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
